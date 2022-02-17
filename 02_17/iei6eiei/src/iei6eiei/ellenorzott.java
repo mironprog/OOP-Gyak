4284f3010b1b86947f1b6596fbd86a707fcd5b16
mironprog <90308606+mironprog@users.noreply.github.com>
2022-02-17 09:37:40 +0100

    Add files via upload

---
+++ 02_17/iei6eiei/src/iei6eiei/ellenorzott.java
@@ -0,0 +1,17 @@
+package iei6eiei;
+
+import java.util.Scanner;
+
+public class ellenorzott {
+	
+	//static Scanner sc = new scanner(System.in);
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		String text = sc.nextLine();
+		System.out.println(text);
+		sc.close();
+		
+	}
+
+}
---
+++ 02_17/iei6eiei/src/iei6eiei/mind1.java
@@ -0,0 +1,31 @@
+package iei6eiei;
+
+public class mind1 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10];
+		int atlag;
+		int osszeg = 0;
+		for(int i = 0; i < arr.length; i++){
+			arr[i] = (int)Math.random() * 100;
+			}
+		
+		for(int i = 0; i < arr.length; i++){
+		System.out.println(arr[i]);
+		}
+		
+		for(int i = arr.length; i > 0; i++){
+		System.out.println(arr[i]);
+		}
+		
+		
+		for(int i = 0; i < arr.length; i++){
+			if (arr[i] % 2 ==0){
+				
+				
+			
+		}
+		}
+	}
+
+}
