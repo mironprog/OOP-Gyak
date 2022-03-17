cc57f950afe716da976337f9e2981861fb1f2d0c
mironprog <90308606+mironprog@users.noreply.github.com>
2022-03-17 09:39:33 +0100

    Add files via upload

---
+++ Gyak6.2/src/myPackage/first/Szam.java
@@ -0,0 +1,25 @@
+package myPackage.first;
+
+public class Szam {
+	static double a = 0.001;
+	private double b;
+
+	public Szam(double b){
+		this.b = b;
+	}
+	
+	public double getB() {
+		return b;
+	}
+
+	public void setB(double b) {
+		this.b = b;
+	}
+	
+	/*public boolean turesenBelul(double b){
+		this.b b
+	}*/
+
+}
+
+
---
+++ Gyak6.2/src/myPackage/second/Runner.java
@@ -0,0 +1,18 @@
+package myPackage.second;
+
+import java.util.Random;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		final double random = 0.5;
+		Random rand = new Random();
+		double doubleRandom[];
+		System.out.println("Random double value between 0.0 and 1.0 : "+doubleRandom);
+		
+		for(int i = 0; i < doubleRandom.length; i++){
+			
+		}
+	}
+
+}
